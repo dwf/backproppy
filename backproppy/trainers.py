9582f49ea81c94e7a1e2b4a25ea07265058405f9
David Warde-Farley <dwf@cs.toronto.edu>
2009-12-08 22:50:20 -0500

    Added momentum to the batch trainer.

--- backproppy/trainers.py
+++ backproppy/trainers.py
@@ -13,8 +13,9 @@ class BatchNetworkTrainer(object):
         self.cost = cost
         self.decay = decay
 
-    def train(self, niters=500, eta=0.1):
+    def train(self, niters=500, eta=0.1, alpha=0.9):
         """Train a network for a given number of iterations."""
+        previous = None
         for iters in xrange(niters):
             predictions = self.network.fprop(self.inputs)
             cost = self.cost.evaluate(self.targets, predictions)
@@ -22,11 +23,19 @@ class BatchNetworkTrainer(object):
             sys.stdout.flush()
             cost_gradient = self.cost.grad(self.targets, predictions)
             net_gradients = self.network.grad(cost_gradient, self.inputs)
-            
-            for layer, gradient in izip(self.network.layers[::-1], net_gradients):
-                if gradient is not None:
-                    decay = 2 * self.decay * layer.params
-                    layer.params -= eta * gradient.reshape(layer.params.shape)
-                    layer.params -= eta * decay
-
+            step = net_gradients.copy()
+            step += 2 * self.decay * self.network.params
+            step *= -eta
+            if previous is not None:
+                step *= (1. - alpha)
+                step += alpha * previous
+            else:
+                previous = np.empty(step.shape)
+            self.network.params += step
+            previous[...] = step
+            #for layer, gradient in izip(self.network.layers[::-1], net_gradients):
+            #    if gradient is not None:
+            #        decay = 2 * self.decay * layer.params
+            #        layer.params -= eta * gradient.reshape(layer.params.shape)
+            #        layer.params -= eta * decay
 
